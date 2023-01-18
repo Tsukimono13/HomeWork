7bad716574db70a8a4b420a622bbab3f90590c04
Irina Litvinova <neforsha@yandex.ru>
2023-01-18 15:45:43 +0300

    fix HW3

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -30,7 +30,7 @@ const Greeting: React.FC<GreetingPropsType> = (
     return (
         <div id={'hw3-form'} className={s.greetingForm}>
             <div className={s.text}>
-                {`Людей добавили: ${lastUserName}`}
+                {'Людей добавили: '}
                 <span id={'hw3-users-total'}>
                     {totalUsers}
                 </span>
